0fd4e3c5e9ef0682329993a91b965baad9471429
giuliarappo <rappogiulia@gmail.com>
2022-12-22 15:00:09 +0100

    edit logic

--- main.js
+++ main.js
@@ -5,15 +5,22 @@ let submitButtun = document.querySelector("#submit");
 let game = document.querySelector("#game");
 let restart = document.querySelector("#restart");
 let circle = document.querySelector("#circle");
+let message = document.createElement('p');
 
-let numberOfAttempt = 1;
+let numberOfAttempt = 0;
 
 startButtun.addEventListener('click', guessTheNumber);
+submitButtun.addEventListener('click', result);
+restart.addEventListener('click', guessTheNumber);
 
 console.log(startButtun);
 
 
 function guessTheNumber() {
+    message.innerText = "";
+    numberChosen.value = "";
+    restart.style.display = "none";
+    submitButtun.style.display = "block";
     startButtun.style.display = "none";
     game.style.display = "block";
     number = Math.floor(Math.random() * 10 + 1);
@@ -28,19 +35,33 @@ function result(event) {
     let numberChoosen = document.getElementById("number-chosen").value;
     
     if (numberChoosen == number) {
-        console.log("You win!")
+        message.innerText = "Congratulation, you win!"
+        message.style.color = 'green';
+        game.appendChild(message);
+        submitButtun.style.display = "none";
+        restart.style.display = "block";
+        
+        return;
     } else if (numberChoosen > number){
+        message.innerText = "Too big!"
+        message.style.color = 'red';
+        game.appendChild(message);
         console.log("Too big!")
     } else if (numberChoosen < number){
+        message.innerText = "Too small!"
+        message.style.color = 'red';
+        game.appendChild(message);
         console.log("Too small!")
     }
-    console.log(`The number is: ${number}`)
+    
 
     if (numberOfAttempt < 3) {
-        console.log(`Hai ancora ${numberOfAttempt} prova ancora!`)
+        console.log(`3 - ${numberOfAttempt} = ${3 - numberOfAttempt}`)
+        console.log(`You have ${3 - numberOfAttempt} attempts yet, prova ancora!`)
     } else {
-        console.log("You loose!")
+        message.innerText = "You lose!"
+        message.style.color = 'red';
+        submitButtun.style.display = "none";
         restart.style.display = "block";
-        submitButtun.disabled = true;
     }
 }
